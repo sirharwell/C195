170217fd2cd8d56b8f99fac026149a18ebf7445f
sirharwell <36307972+sirharwell@users.noreply.github.com>
2021-09-06 17:18:00 -0600

    Added save Division number

--- Scheduling/src/utils/DBCustomer.java
+++ Scheduling/src/utils/DBCustomer.java
@@ -266,20 +266,228 @@ public class DBCustomer {
             return null;
         }
     }
-    
+    static int s;
     public static boolean saveCustomer( int id, String customerName, String address, String phone, String postalCode, String stateChoice, String countryChoice) {
         try {
             Connection conn = DBConnection.getConnection();
             DBQuery.setStatement(conn);
             Statement statement = DBQuery.getStatement();
+            String i = stateChoice; 
+            
+            if (i == "Alabama") {
+            int s = 1; 
+            }
+            else if (i == "Arizona") {
+            int s = 2;
+            }
+            else if (i == "Arkansas") {
+            int s = 3;
+            }
+            else if (i == "California") {
+            int s = 4;
+            }
+            else if (i == "Colorado") {
+            int s = 5;
+            }
+            else if (i == "Connecticut") {
+            int s = 6;
+            }
+            else if (i == "Delaware") {
+            int s = 7;
+            }
+            else if (i == "District of Columbia") {
+            int s = 8;
+            }
+            else if (i == "Florida") {
+            int s = 9;
+            }
+            else if (i == "Georgia") {
+            int s = 10;
+            }
+            else if (i == "Idaho") {
+            int s = 11;
+            }
+            else if (i == "Illinois") {
+            int s = 12;
+            }
+            else if (i == "Indiana") {
+            int s = 13;
+            }
+            else if (i == "Iowa") {
+            int s = 14;
+            }
+            else if (i == "Kansas") {
+            int s = 15;
+            }
+            else if (i == "Kentucky") {
+            int s = 16;
+            }
+            else if (i == "Louisiana") {
+            int s = 17;
+            }
+            else if (i == "Maine") {
+            int s = 18;
+            }
+            else if (i == "Maryland") {
+            int s = 19;
+            }
+            else if (i == "Massachusetts") {
+            int s = 20;
+            }
+            else if (i == "Michigan") {
+            int s = 21;
+            }
+            else if (i == "Minnesota") {
+            int s = 22;
+            }
+            else if (i == "Mississippi") {
+            int s = 23;
+            }
+            else if (i == "Missouri") {
+            int s = 24;
+            }
+            else if (i == "Montana") {
+            int s = 25;
+            }
+            else if (i == "Nebraska") {
+            int s = 26;
+            }
+            else if (i == "Nevada") {
+            int s = 27;
+            }
+            else if (i == "New Hampshire") {
+            int s = 28;
+            }
+            else if (i == "New Jersey") {
+            int s = 29;
+            }
+            else if (i == "New Mexico") {
+            int s = 30;
+            }
+            else if (i == "New York") {
+            int s = 31;
+            }
+            else if (i == "North Carolina") {
+            int s = 32;
+            }
+            else if (i == "North Dakota") {
+            int s = 33;
+            }
+            else if (i == "Ohio") {
+            int s = 34;
+            }
+            else if (i == "Oklahoma") {
+            int s = 35;
+            }
+            else if (i == "Oregon") {
+            int s = 36;
+            }
+            else if (i == "Pennslyvania") {
+            int s = 37;
+            }
+            else if (i == "Rhode Island") {
+            int s = 38;
+            }
+            else if (i == "South Carolina") {
+            int s = 39;
+            }
+            else if (i ==  "South Dakota") {
+            int s = 40;
+            }
+            else if (i == "Tennessee") {
+            int s = 41;
+            }
+            else if (i == "Texas") {
+            int s = 42;
+            }
+            else if (i == "Utah") {
+            int s = 43;
+            }
+            else if (i == "Vermont") {
+            int s = 44;
+            }
+            else if (i == "Virginia") {
+            int s = 45;
+            }
+            else if (i == "Washington") {
+            int s = 46;
+            }
+            else if (i == "West Virginia") {
+            int s = 47;
+            }
+            else if (i == "Wisconsin") {
+            int s = 48;
+            }
+            else if (i == "Wyoming") {
+            int s = 49;
+            }
+            else if (i == "Hawaii") {
+            int s = 2;
+            }
+            else if (i == "Alaska") {
+            int s = 54;
+            }
+            else if (i == "Northwest Territory") {
+            int s = 60;
+            }
+            else if (i == "Alberta") {
+            int s = 61;
+            }
+            else if (i == "British Columbia") {
+            int s = 62;
+            }
+            else if (i == "Manitoba") {
+            int s = 63;
+            }
+            else if (i == "New Brunswick") {
+            int s = 64;
+            }
+            else if (i == "Nova Scotia") {
+            int s = 65;
+            }
+            else if (i == "Prince Edward Island") {
+            int s = 66;
+            }
+            else if (i == "Ontario") {
+            int s = 67;
+            }
+            else if (i == "Quebec") {
+            int s = 68;
+            }
+            else if (i == "Saskatchewan") {
+            int s = 69;
+            }
+            else if (i == "Nunavut") {
+            int s = 70;
+            }
+            else if (i == "Yukon") {
+            int s = 71;
+            }
+            else if (i == "Newfoundland and Labrador") {
+            int s = 72;
+            }
+            else if (i == "England") {
+            int s = 101;
+            }
+            else if (i == "Wales") {
+            int s = 102;
+            }
+            else if (i == "Scotland") {
+            int s = 103;
+            }
+            else if (i == "Northern Ireland") {
+            int s = 104;
+            };
+            
+            
             String query = "INSERT INTO customers(Customer_ID, Customer_Name, Address, Phone, Postal_Code, Division_ID)" + 
                     "VALUES(" +
                     "'" + id + "'," +
                     "'" + customerName + "'," +
-                    "'" + address + " " + stateChoice + " " + countryChoice + "'," +
+                    "'" + address + "'," +
                     "'" + phone + "'," + 
                     "'" + postalCode + "'," +
-                    "15" +
+                    s +
                     ")";
             statement.execute(query);
         } catch (SQLException e) {
